a386b01e7079969ca654df1c3aa5be9fe82b3b42
rachel <rachel@DESKTOP-GL6B5C0>
2018-02-15 11:49:32 -0700

    2/15/18 Created glazer calc app

--- calcApp/MainPage.xaml.cs
+++ calcApp/MainPage.xaml.cs
@@ -26,5 +26,73 @@ namespace calcApp
         {
             this.InitializeComponent();
         }
+
+      
+        private void txtWidth_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void txtHeight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = txtWidth.Text;
+            heightString = txtHeight.Text;
+
+            if ((widthString == "") || (heightString == ""))
+            {
+                ErrorDisplay();
+                return;
+            }
+           
+            width = double.Parse(widthString);
+            height = double.Parse(heightString);
+
+
+            if ((width > 0 && width < 40) && (height > 0 && height < 40))
+            {
+                woodLength = 2 * (width + height) * 3.25;
+                glassArea = 2 * (width * height);
+                txtWoodLength.Text = woodLength.ToString();
+                txtGlassArea.Text = glassArea.ToString();
+                txtWidthOutput.Text = width.ToString();
+                txtHeightOutput.Text = height.ToString();
+
+                txtHeightOutput.Visibility = Visibility.Visible;
+                txtWidthOutput.Visibility = Visibility.Visible;
+                txtWoodLength.Visibility = Visibility.Visible;
+                txtGlassArea.Visibility = Visibility.Visible;
+                txtWarning.Visibility = Visibility.Collapsed;
+                dateBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ErrorDisplay();
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {            
+            txtQuantity.Text = "Quantity: " + mySlider.Value.ToString();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        public void ErrorDisplay()
+        {
+            txtHeightOutput.Visibility = Visibility.Collapsed;
+            txtWidthOutput.Visibility = Visibility.Collapsed;
+            txtWarning.Visibility = Visibility.Visible;
+            txtWoodLength.Visibility = Visibility.Collapsed;
+            txtGlassArea.Visibility = Visibility.Collapsed;
+            dateBox.Visibility = Visibility.Collapsed;
+        }
     }
 }
